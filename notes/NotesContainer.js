5951182a66aa4cc2338e34cf9e555357583fbd7a
Erwin Majewski <erwin.majewski@gmail.com>
2024-10-08 21:15:49 +0200

    Render items as DOM elements

--- notes/NotesContainer.js
+++ notes/NotesContainer.js
@@ -51,4 +51,44 @@ export class NotesContainer {
             ...details
         };
     };
+
+    renderItems() {
+        const container = document.getElementById('notesContainer');
+
+        const noteItems = this.notes.map(note => {
+            const noteContainer = document.createElement('div');
+            noteContainer.classList.add('note-item');
+
+            /* INPUT FORMS */
+            const inputFormsContainer = document.createElement('div');
+            inputFormsContainer.classList.add('note-item-inputs');
+
+            const noteTitle = document.createElement('div');
+            noteTitle.textContent = note.title;
+            inputFormsContainer.appendChild(noteTitle);
+
+            const noteBody = document.createElement('div');
+            noteBody.textContent = note.body;
+            inputFormsContainer.appendChild(noteBody);
+
+            const creationDate = document.createElement('div');
+            creationDate.textContent = note.date;
+            inputFormsContainer.appendChild(creationDate);
+
+            /* ACTION BUTTONS */
+            const deleteNoteButton = document.createElement('div');
+            deleteNoteButton.textContent = 'delete';
+
+            const editNoteButton = document.createElement('div');
+            editNoteButton.textContent = 'edit';
+
+            noteContainer.appendChild(inputFormsContainer);
+            noteContainer.appendChild(editNoteButton);
+            noteContainer.appendChild(deleteNoteButton);
+
+            return noteContainer;
+        });
+
+        noteItems.forEach(element => container.appendChild(element));
+    }
 };
