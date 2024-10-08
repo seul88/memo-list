d619666af906623b4099fc0f4107278169ddf85b
Erwin Majewski <erwin.majewski@gmail.com>
2024-10-08 21:34:26 +0200

    Delegate layout render logic to LayoutHandler class

--- notes/NotesContainer.js
+++ notes/NotesContainer.js
@@ -52,43 +52,43 @@ export class NotesContainer {
         };
     };
 
-    renderItems() {
-        const container = document.getElementById('notesContainer');
+    // renderItems() {
+    //     const container = document.getElementById('notesList');
 
-        const noteItems = this.notes.map(note => {
-            const noteContainer = document.createElement('div');
-            noteContainer.classList.add('note-item');
+    //     const noteItems = this.notes.map(note => {
+    //         const noteContainer = document.createElement('div');
+    //         noteContainer.classList.add('note-item');
 
-            /* INPUT FORMS */
-            const inputFormsContainer = document.createElement('div');
-            inputFormsContainer.classList.add('note-item-inputs');
+    //         /* INPUT FORMS */
+    //         const inputFormsContainer = document.createElement('div');
+    //         inputFormsContainer.classList.add('note-item-inputs');
 
-            const noteTitle = document.createElement('div');
-            noteTitle.textContent = note.title;
-            inputFormsContainer.appendChild(noteTitle);
+    //         const noteTitle = document.createElement('div');
+    //         noteTitle.textContent = note.title;
+    //         inputFormsContainer.appendChild(noteTitle);
 
-            const noteBody = document.createElement('div');
-            noteBody.textContent = note.body;
-            inputFormsContainer.appendChild(noteBody);
+    //         const noteBody = document.createElement('div');
+    //         noteBody.textContent = note.body;
+    //         inputFormsContainer.appendChild(noteBody);
 
-            const creationDate = document.createElement('div');
-            creationDate.textContent = note.date;
-            inputFormsContainer.appendChild(creationDate);
+    //         const creationDate = document.createElement('div');
+    //         creationDate.textContent = note.date;
+    //         inputFormsContainer.appendChild(creationDate);
 
-            /* ACTION BUTTONS */
-            const deleteNoteButton = document.createElement('div');
-            deleteNoteButton.textContent = 'delete';
+    //         /* ACTION BUTTONS */
+    //         const deleteNoteButton = document.createElement('div');
+    //         deleteNoteButton.textContent = 'delete';
 
-            const editNoteButton = document.createElement('div');
-            editNoteButton.textContent = 'edit';
+    //         const editNoteButton = document.createElement('div');
+    //         editNoteButton.textContent = 'edit';
 
-            noteContainer.appendChild(inputFormsContainer);
-            noteContainer.appendChild(editNoteButton);
-            noteContainer.appendChild(deleteNoteButton);
+    //         noteContainer.appendChild(inputFormsContainer);
+    //         noteContainer.appendChild(editNoteButton);
+    //         noteContainer.appendChild(deleteNoteButton);
 
-            return noteContainer;
-        });
+    //         return noteContainer;
+    //     });
 
-        noteItems.forEach(element => container.appendChild(element));
-    }
+    //     noteItems.forEach(element => container.appendChild(element));
+    // }
 };
