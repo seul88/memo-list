a841d170e27b144360de92ba1f88572374620f7d
Erwin Majewski <erwin.majewski@gmail.com>
2024-10-10 15:07:36 +0200

    adjust add note form styles

--- notes/AddNoteView.js
+++ notes/AddNoteView.js
@@ -46,6 +46,8 @@ export class AddNoteView extends HTMLElement {
 
     onCancelClick() {
         this._shouldrender = false;
+        this._title = '';
+        this._body = '';
         this.render();
     }
 
@@ -63,9 +65,11 @@ export class AddNoteView extends HTMLElement {
             title: this.title,
             body: this.body, 
             date: Date.now()
-        })
+        });
 
         this._shouldrender = false;
+        this._title = '';
+        this._body = '';
         this.render();
     }
 
@@ -77,23 +81,40 @@ export class AddNoteView extends HTMLElement {
             }
             
             const container = document.createElement('div');
-            container.id = 'addNoteForm'
+            container.id = 'addNoteForm';
+
+            const headerRow = document.createElement('div');
+            headerRow.classList.add('add-note-title-row');
+
+            const headerText = document.createElement('div');
+            headerText.textContent = 'Add new note';
+            headerText.classList.add('add-note-header-text');
+            headerRow.appendChild(headerText);
 
             const cancelButton = document.createElement('button');
             cancelButton.textContent = 'Cancel';
             cancelButton.onclick = () => this.onCancelClick();
-            container.appendChild(cancelButton);
+            cancelButton.classList.add('add-note-cancel-button');
+            headerRow.appendChild(cancelButton);
+
+            container.appendChild(headerRow);
     
             const titleForm = document.createElement('input');
             titleForm.oninput = (e) => this.onTitleChange(e.target.value);
+            titleForm.placeholder = 'Note title';
+            titleForm.classList.add('and-note-input-form');
             container.appendChild(titleForm);
     
-            const bodyForm = document.createElement('input');
+            const bodyForm = document.createElement('textarea');
+            bodyForm.rows = 7;
             bodyForm.oninput = (e) => this.onBodyChange(e.target.value);
+            bodyForm.placeholder = 'Your note';
+            bodyForm.classList.add('and-note-input-form');
             container.appendChild(bodyForm);
     
             const saveButton = document.createElement('button');
             saveButton.textContent = 'Save';
+            saveButton.classList.add('add-note-save-button');
             saveButton.onclick = () => this.onSaveClick();
             container.appendChild(saveButton);
     
