3ffd2093b0afed96b9dd616dd4b6001a1ff2efbd
Erwin Majewski <erwin.majewski@gmail.com>
2024-10-11 16:04:11 +0200

    style adjustments

--- mainHandler.js
+++ mainHandler.js
@@ -1,3 +1,4 @@
+import { displayNoNotesMessage } from './notes/dom-actions/no-notes/displayNoNotesMessage.js';
 import { NotesContainer } from './notes/views/container/NotesContainer.js';
 import { AddNoteView } from './notes/views/create/AddNoteView.js';
 import { EditNoteView } from './notes/views/edit/EditNoteView.js'
@@ -5,31 +6,6 @@ import { EditNoteView } from './notes/views/edit/EditNoteView.js'
 export const mainHandler = () => {
   customElements.define('notes-container', NotesContainer);
   const notes = document.querySelector('notes-container');
-
-  /* Setup mock data */
-  notes.addNote({
-    title: 'Test 1',
-    body: 'Test value input',
-    date: Date.now()
-  });
-
-  notes.addNote({
-    title: 'Test 2',
-    body: 'Much more text value input',
-    date: Date.now()
-  });
-
-  notes.addNote({
-    title: 'Test 3',
-    body: 'Much more text value input x2 Much more text value input',
-    date: Date.now()
-  });
-
-  notes.addNote({
-    title: 'Test 4',
-    body: 'Random poem about shopping list',
-    date: Date.now()
-  });
 
   const searchFormElement = document.getElementById('searchForm');
   const inputHandler = function(e) {
@@ -46,4 +22,6 @@ export const mainHandler = () => {
   addNoteButton.addEventListener('click', addNote);
 
   customElements.define('edit-note-view', EditNoteView);
+
+  displayNoNotesMessage();
 };
